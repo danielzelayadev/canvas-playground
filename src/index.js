7f0188d0c919f56d0285b17d88f39306b16a567a
Daniel Zelaya <wupaz9@gmail.com>
2017-10-13 23:19:39 -0600

    Wire up form submit and canvas rendering

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-render("canvas-form");
+function onCanvasFormSubmit(e) {
+  const width  = Number(document.querySelector("#width-input").nodeValue);
+  const height = Number(document.querySelector("#height-input").nodeValue);
+
+  e.preventDefault();
+
+  if (width < 0 || height < 0)
+    alert("Width and height must be >= 0! >:(");
+  else
+    render("main")
+      .then(() => setupCanvas(width, height))
+      .catch(renderError);
+}
+
+function setupCanvas(w = 0, h = 0) {
+  const canvas = document.querySelector("canvas");
+
+  canvas.setAttribute("width", w.toString());
+  canvas.setAttribute("height", h.toString());
+}
+
+render("canvas-form")
+  .then(() => {
+    document
+      .querySelector("form")
+      .addEventListener("submit", onCanvasFormSubmit);
+  })
+  .catch(renderError);
