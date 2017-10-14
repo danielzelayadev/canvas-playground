cad79cce649876e820b9c6e531c37f52fd2b68c1
Daniel Zelaya <wupaz9@gmail.com>
2017-10-14 00:27:02 -0600

    Doing weird stuff

--- src/index.js
+++ src/index.js
@@ -20,8 +20,31 @@ function setupCanvas(w = 0, h = 0) {
   canvas.setAttribute("height", h.toString());
   canvas.style = "border: 1px solid #000";
 
+  const speed = 5;
+  let x = 220, y = 270;
+
+  canvas.addEventListener("keypress", e => {
+    if (e.key == "w")
+      y -= speed;
+    if (e.key == "d")
+      x += speed;
+    if (e.key == "a")
+      x -= speed;
+    if (e.key == "s")
+      y += speed;
+      
+    renderPlayer(ctx, x, y);
+  });
+
   ctx.fillStyle = 'blue';
   ctx.fillRect(30, 60, 400, 300);
+
+  renderPlayer(ctx, x, y);
+}
+
+function renderPlayer(ctx, x, y) {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(x, y, 400, 300);
 }
 
 function renderCanvasForm() {
@@ -34,4 +57,6 @@ function renderCanvasForm() {
     .catch(renderError);
 }
 
-renderCanvasForm();
+render("main")
+  .then(() => setupCanvas(650, 600))
+  .catch(renderError);
